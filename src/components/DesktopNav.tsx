fe5a7938adb50deed17518650caa7f7050c0fe17
donado10 <adamscoulibaly10@gmail.com>
2025-02-14 13:21:48 +0000

    update

--- src/components/DesktopNav.tsx
+++ src/components/DesktopNav.tsx
@@ -6,8 +6,8 @@ import Image from "next/image";
 import IconHome from "@/assets/images/icon-home.svg";
 import IconSearch from "@/assets/images/icon-search.svg";
 import IconArchive from "@/assets/images/icon-archive.svg";
-import IconTag from "@/assets/images/icon-tag.svg";
-import IconSetting from "@/assets/images/icon-settings.svg";
+import { IoIosArrowForward } from "react-icons/io";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -37,12 +37,17 @@ const DesktopNav = () => {
             <Link
               key={i}
               href={link.nav}
-              className={`flex w-full rounded-md px-1 py-2 ${location === link.nav ? "bg-notes-blue-third" : ""}`}
+              className={`flex w-full items-center justify-between rounded-md px-1 py-2 ${location === link.nav ? "bg-notes-blue-third" : ""}`}
             >
               <div className="flex w-3/4 items-center gap-2 py-1">
                 {link.logo}
                 <h2 className="text-xs">{link.name}</h2>
               </div>
+              {location === link.nav && (
+                <div>
+                  <IoIosArrowForward />
+                </div>
+              )}
             </Link>
           );
         })}
