5d7b5cac76a12bf4c35246df3563477a196d6762
donado10 <adamscoulibaly10@gmail.com>
2025-02-27 14:26:17 +0000

    update

--- src/components/Note/DesktopScreen.tsx
+++ src/components/Note/DesktopScreen.tsx
@@ -13,18 +13,6 @@ import { NoteProvider } from "./NoteContent";
 
 const DesktopScreen = () => {
   const note = useContext(NoteProvider)?.note!;
-  const notesCtx = useContext(NotesProvider);
-  /* 
-  useEffect(() => {
-    const actualNote = notesCtx.data.filter((note_) => note_._id === note._id);
-    if (JSON.stringify(note) !== JSON.stringify(actualNote[0])) {
-      notesCtx.editedNote!({ ...note });
-    } else {
-      notesCtx.editedNote!(null);
-    }
-
-    return;
-  }, [JSON.stringify(note)]); */
 
   return (
     <div className="flex h-full flex-col gap-4 p-4">
