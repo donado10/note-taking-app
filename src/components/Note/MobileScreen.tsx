f0211f87c419d7b6f8f057033d7acd92bddf8742
donado10 <adamscoulibaly10@gmail.com>
2025-03-17 14:58:31 +0000

    update

--- src/components/Note/MobileScreen.tsx
+++ src/components/Note/MobileScreen.tsx
@@ -14,9 +14,11 @@ import {
 import { INote } from "@/models/noteModel";
 import { NotesProvider } from "@/context/NotesContext";
 import { NoteProvider } from "./NoteContent";
+import { useRouter } from "next/navigation";
 
 const MobileScreen = () => {
   const note = useContext(NoteProvider)?.note!;
+  const notesCtx = useContext(NotesProvider);
 
   const [noteMetadata, setNoteMetadata] = useState<INoteMetadata | null>({
     tags: note.tags.join(","),
@@ -28,16 +30,63 @@ const MobileScreen = () => {
 
   const [saveTrigger, setSaveTrigger] = useState(false);
   const [cancelTrigger, setCancelTrigger] = useState(false);
+  const router = useRouter();
+
+  const saveHandler = async () => {
+    const new_note: INote = {
+      ...note,
+      lastEdited: noteMetadata?.lastEdited ?? "",
+      title: noteMetadata?.title ?? "",
+      tags: noteMetadata?.tags.split(",") ?? [],
+      content: noteContent ?? "",
+    };
+
+    const last_note = { ...note };
+
+    const response = await fetch("http://localhost:3000/api/notes", {
+      method: "PUT",
+      body: JSON.stringify({ notes: new_note }),
+    });
+
+    if (response.status === 200) {
+      router.back();
+      router.refresh();
+      notesCtx.editNotes!([
+        ...notesCtx.data.filter((note) => note._id !== new_note._id),
+        new_note,
+      ]);
+    }
+  };
+
+  const cancelHandler = () => {
+    const new_note: INote = {
+      ...note,
+      lastEdited: noteMetadata?.lastEdited ?? "",
+      title: noteMetadata?.title ?? "",
+      tags: noteMetadata?.tags.split(",") ?? [],
+      content: noteContent ?? "",
+    };
+
+    const last_note = { ...note };
+
+    setNoteMetadata({
+      title: note?.title ?? "",
+      tags: note?.tags.join(",") ?? "",
+      lastEdited: note?.lastEdited ?? "",
+    });
+
+    setNoteContent(note.content ?? "");
+  };
 
   useEffect(() => {
     if (saveTrigger) {
-      console.log("saving...");
+      saveHandler();
       setSaveTrigger(false);
       return;
     }
 
     if (cancelTrigger) {
-      console.log("cancel...");
+      cancelHandler();
       setCancelTrigger(false);
       return;
     }
